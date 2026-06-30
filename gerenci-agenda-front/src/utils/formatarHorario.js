520b410f9a036a4e3ac183cee8a3329a8831a442
Kauzz <kaynnanjeckson@gmail.com>
2026-06-30 16:58:37 -0300

    temporario

--- gerenci-agenda-front/src/utils/formatarHorario.js
+++ gerenci-agenda-front/src/utils/formatarHorario.js
@@ -1,11 +1,13 @@
 export function formatarHorario(dataISO) {
-    const data = new Date(dataISO);
 
-    const dia = String(data.getUTCDate()).padStart(2, "0");
-    const mes = String(data.getUTCMonth() + 1).padStart(2, "0");
-
-    const hora = String(data.getUTCHours()).padStart(2, "0");
-    const minuto = String(data.getUTCMinutes()).padStart(2, "0");
-
-    return `${dia}/${mes} ${hora}:${minuto}`;
+    console.log(dataISO)
+    console.log(new Date(dataISO));
+    return new Intl.DateTimeFormat("pt-BR", {
+        day: "2-digit",
+        month: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false,
+        timeZone: "America/Fortaleza",
+    }).format(new Date(dataISO));
 }
